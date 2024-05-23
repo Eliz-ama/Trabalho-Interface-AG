d8b129676bd4f165f0016f2960fac16c2bc5253a
Eliz-ama <104949111+Eliz-ama@users.noreply.github.com>
2024-05-23 19:22:23 -0300

    tentando

--- app/(tabs)/ResultScreen.tsx
+++ app/(tabs)/ResultScreen.tsx
@@ -1,19 +1,33 @@
-// Importe apenas o necessário do react-native
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const ResultScreen = ({ route }) => {
-  // Lógica para extrair os resultados do route.params
+  const { result } = route.params;
 
   return (
     <View style={styles.container}>
-      {/* Conteúdo do componente ResultScreen */}
+      <Text style={styles.resultText}>Informações da base:</Text>
+      <Text style={styles.resultText}>- Quantidade de instâncias: {result.instances}</Text>
+      <Text style={styles.resultText}>- Quantidade de classes: {result.classes}</Text>
+      <Text style={styles.resultText}>- Quantidade de atributos: {result.attributes}</Text>
+      <Text style={styles.resultText}>Informações do algoritmo:</Text>
+      <Text style={styles.resultText}>- Algoritmo: {result.algorithm}</Text>
+      <Text style={styles.resultText}>- Taxa de acerto: {result.accuracy}%</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // Estilos do componente ResultScreen
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+  },
+  resultText: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
 });
 
 export default ResultScreen;
